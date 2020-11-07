6b1ff41496b00a7f167a9887a1427db520fc03e5
Andrew Madonna <andrew.w.madonna@gmail.com>
2020-11-07 12:56:20 -0500

    Refactor ZIP shp and gdb handling

--- tap_files/format_handlers/gis.py
+++ tap_files/format_handlers/gis.py
@@ -1,4 +1,7 @@
 import io
+import os
+import zipfile
+import tempfile
 
 import orjson
 import singer
@@ -9,65 +12,10 @@ from tap_files.discover_utils import SDC_SOURCE_LINENO_COLUMN
 LOGGER = singer.get_logger()
 
 try:
-    import fiona
     import geopandas
-    from geopandas import GeoDataFrame, GeoSeries
-    from shapely.geometry.base import BaseGeometry
     from shapely.geometry import mapping, Polygon
-
-    try:
-        from fiona import Env as fiona_env
-    except ImportError:
-        from fiona import drivers as fiona_env
 except:
     pass
-
-## from https://github.com/geopandas/geopandas/blob/master/geopandas/io/file.py
-## it's currently broken for file-like object ZIPs
-def _read_file_like(obj, bbox=None, mask=None, rows=None, **kwargs):
-    with fiona_env():
-        with fiona.BytesCollection(obj.read(), **kwargs) as features:
-
-            # In a future Fiona release the crs attribute of features will
-            # no longer be a dict, but will behave like a dict. So this should
-            # be forwards compatible
-            crs = (
-                features.crs["init"]
-                if features.crs and "init" in features.crs
-                else features.crs_wkt
-            )
-
-            # handle loading the bounding box
-            if bbox is not None:
-                if isinstance(bbox, (GeoDataFrame, GeoSeries)):
-                    bbox = tuple(bbox.to_crs(crs).total_bounds)
-                elif isinstance(bbox, BaseGeometry):
-                    bbox = bbox.bounds
-                assert len(bbox) == 4
-            # handle loading the mask
-            elif isinstance(mask, (GeoDataFrame, GeoSeries)):
-                mask = mapping(mask.to_crs(crs).unary_union)
-            elif isinstance(mask, BaseGeometry):
-                mask = mapping(mask)
-            # setup the data loading filter
-            if rows is not None:
-                if isinstance(rows, int):
-                    rows = slice(rows)
-                elif not isinstance(rows, slice):
-                    raise TypeError("'rows' must be an integer or a slice.")
-                f_filt = features.filter(
-                    rows.start, rows.stop, rows.step, bbox=bbox, mask=mask
-                )
-            elif any((bbox, mask)):
-                f_filt = features.filter(bbox=bbox, mask=mask)
-            else:
-                f_filt = features
-            # get list of columns
-            columns = list(features.schema["properties"])
-
-            return GeoDataFrame.from_features(
-                f_filt, crs=crs, columns=columns + ["geometry"]
-            )
 
 class GISFormatHandler(BaseFormatHandler):
     format_name = 'gis'
@@ -80,6 +28,7 @@ class GISFormatHandler(BaseFormatHandler):
         skip_lines = format_options.get('skip_lines')
         file_gis_format = format_options.get('file_gis_format', 'shp')
         is_shp_archive = format_options.get('is_shp_archive', True)
+        layer = format_options.get('layer')
         geom_fieldname = format_options.get('geom_fieldname', 'geom')
         geom_format = format_options.get('geom_format', 'geojson_string')
         to_crs = format_options.get('to_crs')
@@ -100,11 +49,24 @@ class GISFormatHandler(BaseFormatHandler):
                     record[SDC_SOURCE_LINENO_COLUMN] = line_num
                     yield record
         else:
-            if file_gis_format == 'shp' and is_shp_archive:
-                df = _read_file_like(file)
-            elif file_gis_format == 'shp' or ext == 'shp':
-                df = geopandas.read_file(file)
+            if file_gis_format in ['shp', 'gdb']:
+                if is_shp_archive:
+                    with zipfile.ZipFile(file) as zip_:
+                        dir_suffix = None
+                        if file_gis_format == 'gdb':
+                            dir_suffix = '.gdb'
+                        with tempfile.TemporaryDirectory(suffix=dir_suffix) as tempdir:
+                            for zip_info in zip_.infolist():
+                                if zip_info.filename[-1] == '/':
+                                    continue
+                                zip_info.filename = os.path.basename(zip_info.filename)
+                                zip_.extract(zip_info, tempdir)
+                            df = geopandas.read_file(tempdir, layer=layer)
+                else:
+                    df = geopandas.read_file(file, layer=layer)
             elif file_gis_format == 'geojson' or ext in ['json', 'geojson']:
+                if layer is not None:
+                    raise Exception('geojson doens\'t support multiple layers')
                 text_file = io.TextIOWrapper(file, encoding='utf-8')
                 df = geopandas.read_file(text_file)
 
